d97bfafee711b5fae600510f0cbf397705409032
roboPriest <36549072+roboPriest@users.noreply.github.com>
2018-03-02 09:54:03 -0500

    Add files via upload

--- classes.py
+++ classes.py
@@ -1,268 +1,457 @@
-#===========================================================================================
-#Inventory==================================================================================
+# ===========================================================================================
+# Inventory==================================================================================
+
 class inventory():
     'the organization of items held by the player'
+
     def __init__(self, type):
         self.type = type
 
+
 class ownedWeapons(inventory):
+
     def __init__(self, type, weapon1, weapon2, weapon3):
         self.weapon1 = weapon1
+
         self.weapon2 = weapon2
+
         self.weapon3 = weapon3
+
         super().__init__(type)
 
     def encodeownedWeapons(self):
-            global weaponString
-            weaponStats = [self.weapon1, self.weapon2, self.weapon3]
-            weaponString = " ".join([str(i) for i in weaponStats])
+        global weaponString
+
+        weaponStats = [self.weapon1, self.weapon2, self.weapon3]
+
+        weaponString = " ".join([str(i) for i in weaponStats])
+
 
 class ownedArmour(inventory):
+
     def __init__(self, type, armour1, armour2, armour3):
         self.armour1 = armour1
+
         self.armour2 = armour2
+
         self.armour3 = armour3
+
         super().__init__(type)
 
     def encodeownedArmour(self):
         global armourString
+
         armourStats = [self.armour1, self.armour2, self.armour3]
+
         armourString = " ".join([str(i) for i in armourStats])
 
+
 class ownedItems(inventory):
+
     def __init__(self, type, item1, item2, item3):
         self.item1 = item1
+
         self.item2 = item2
+
         self.item3 = item3
+
         super().__init__(type)
 
     def encodeownedItems(self):
         global itemString
+
         itemStats = [[self.item1, self.item2, self.item3]]
+
         itemString = " ".join([str(i) for i in itemStats])
 
+
 class ownedAccessories(inventory):
+
     def __init__(self, type, acc1, acc2, acc3):
         self.acc1 = acc1
+
         self.acc2 = acc2
+
         self.acc3 = acc3
+
         super().__init__(type)
 
     def encodeownedAccessories(self):
         global accessoryString
+
         accessoryStats = [[self.acc1, self.acc2, self.acc3]]
+
         accessoryString = " ".join([str(i) for i in accessoryStats])
 
+
 class equippedWeapons(inventory):
+
     def __init__(self, type, primary):
         self.primary = primary
+
         super().__init__(type)
 
     def encodeequippedWeapons(self):
         global eweaponString
+
         eweaponStats = [self.primary]
+
         eweaponString = " ".join([str(i) for i in eweaponStats])
 
+
 class equippedArmour(inventory):
+
     def __init__(self, type, armour):
         self.armour = armour
+
         super().__init__(type)
 
     def encodeequippedArmour(self):
         global earmourString
+
         earmourStats = [self.armour]
+
         earmourString = " ".join([str(i) for i in earmourStats])
 
+
 class equippedAccessories(inventory):
+
     def __init__(self, type, acc1, acc2, acc3, acc4):
         self.acc1 = acc1
+
         self.acc2 = acc2
+
         self.acc3 = acc3
+
         self.acc4 = acc4
+
         super().__init__(type)
 
     def encodeequippedAccessories(self):
         global accString
+
         accStats = [self.acc1, self.acc2, self.acc3, self.acc4]
+
         accString = " ".join([str(i) for i in accStats])
 
+
 class beltItems(inventory):
+
     def __init__(self, type, pouch1, pouch2, pouch3, pouch4, pouch5, pouch6):
         self.pouch1 = pouch1
+
         self.pouch2 = pouch2
+
         self.pouch3 = pouch3
+
         self.pouch4 = pouch4
+
         self.pouch5 = pouch5
+
         self.pouch6 = pouch6
+
         super().__init__(type)
 
     def encodebeltItems(self):
         global beltString
+
         beltStats = [self.pouch1, self.pouch2, self.pouch3, self.pouch4, self.pouch5, self.pouch6]
+
         beltString = " ".join([str(i) for i in beltStats])
-#=============================================================================================
-#item classes=================================================================================
+
+
+# =============================================================================================
+
+# item classes=================================================================================
+
 class item():
     'objects that can be held by player characters or npcs'
+
     def __init__(self, ID, name, weight, value, desc):
         self.ID = ID
+
         self.name = name
+
         self.weight = weight
-        self.value = value 
+
+        self.value = value
+
         self.desc = desc
-#==============================================================================================
+
+
+# ==============================================================================================
+
 class currency(item):
     'The currency in the world of Thule.'
-    def __init__(self, ID, name, weight, value, desc):
-        super().__init__( ID, name, weight, value, desc)
-    def currencyDescription(self):
-        print ('Name -', self.name)
-        print ('Value -', self.value)
-        print ('Descripton -', self.desc)
 
-#weapon classes--------------------------------------------------------------------------------
+    def __init__(self, ID, name, weight, value, desc):
+        super().__init__(ID, name, weight, value, desc)
+
+    def currencyDescription(self):
+        print('Name -', self.name)
+
+        print('Value -', self.value)
+
+        print('Descripton -', self.desc)
+
+
+# weapon classes--------------------------------------------------------------------------------
+
 class weapon(item):
     'weapons'
-    def __init__(self, ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq, IntReq):
+
+    def __init__(self, ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq,
+                 IntReq):
         self.weaponClass = weaponClass
+
         self.effects = effects
+
         self.move1 = move1
+
         self.move2 = move2
+
         self.move3 = move3
+
         self.StrReq = StrReq
+
         self.DexReq = DexReq
+
         self.IntReq = IntReq
-        super().__init__( ID, name, weight, value, desc)
+
+        super().__init__(ID, name, weight, value, desc)
+
     def weaponStats(self):
         print('Name -', self.name)
+
         print('Weight -', self.weight)
+
         print('Value -', self.value)
+
         print('Descripton -', self.desc)
+
         print('Class -', self.weaponClass)
+
         print('Effects -', self.effects)
+
         print('Move 1 -', self.move1)
+
         print('Move 2 -', self.move2)
+
         print('Move 3 -', self.move3)
+
         print('Str requirement -', self.StrReq)
+
         print('Dex requirement -', self.DexReq)
+
         print('Int requirement -', self.IntReq)
-        
+
+
 class heavyWeapon(weapon):
     'heavy weapons'
-    def __init__(self, ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq, IntReq):
-        super().__init__( ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq, IntReq)
+
+    def __init__(self, ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq,
+                 IntReq):
+        super().__init__(ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq,
+                         IntReq)
+
+
 class lightWeapon(weapon):
     'light weapons'
-    def __init__(self, ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq, IntReq):
-        super().__init__( ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq, IntReq)
+
+    def __init__(self, ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq,
+                 IntReq):
+        super().__init__(ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq,
+                         IntReq)
+
+
 class magicWeapon(weapon):
     'magic weapons'
-    def __init__(self, ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq, IntReq):
-        super().__init__( ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq, IntReq)
-#apparel classes---------------------------------------------------------------------------------
+
+    def __init__(self, ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq,
+                 IntReq):
+        super().__init__(ID, name, weight, value, desc, weaponClass, effects, move1, move2, move3, StrReq, DexReq,
+                         IntReq)
+
+
+# apparel classes---------------------------------------------------------------------------------
+
 class apparel(item):
     'apparel'
+
     def __init__(self, ID, name, weight, value, desc, apparelClass, defense, effects):
         self.apparelClass = apparelClass
+
         self.defense = defense
+
         self.effects = effects
-        super().__init__( ID, name, weight, value, desc)
+
+        super().__init__(ID, name, weight, value, desc)
+
     def apparelStats(self):
         print('Name -', self.name)
+
         print('Weight -', self.weight)
+
         print('Value -', self.value)
+
         print('Descripton -', self.desc)
+
         print('Class -', self.apparelClass)
+
         print('Defense -', self.defense)
+
         print('Effects -', self.effects)
-        
+
+
 class heavyArmour(apparel):
     'heavy armour'
+
     def __init__(self, ID, name, weight, value, desc, apparelClass, defense, effects):
-        super().__init__( ID, name, weight, value, desc, apparelClass, defense, effects)
-        
+        super().__init__(ID, name, weight, value, desc, apparelClass, defense, effects)
+
+
 class lightArmour(apparel):
     'light armour'
-    def __init__(self, ID, name, weight, value, desc, apparelClass, defense, effects):  
-        super().__init__( ID, name, weight, value, desc, apparelClass, defense, effects)
-        
+
+    def __init__(self, ID, name, weight, value, desc, apparelClass, defense, effects):
+        super().__init__(ID, name, weight, value, desc, apparelClass, defense, effects)
+
+
 class clothing(apparel):
     'clothes'
+
     def __init__(self, ID, name, weight, value, desc, apparelClass, defense, effects):
-        super().__init__( ID, name, weight, value, desc, apparelClass, defense, effects)
-        
-#=============================================================================================
-#Accessories==================================================================================
+        super().__init__(ID, name, weight, value, desc, apparelClass, defense, effects)
+
+
+# =============================================================================================
+
+# Accessories==================================================================================
+
 class accessories(item):
     'accessories'
+
     def __init__(self, ID, name, weight, value, desc, accClass, statEffect):
         self.accClass = accClass
+
         self.statEffect = statEffect
-        super().__init__( ID, name, weight, value, desc)
+
+        super().__init__(ID, name, weight, value, desc)
+
     def accessoryStats(self):
         print('Name -', self.name)
+
         print('Weight -', self.weight)
+
         print('Value -', self.value)
+
         print('Descripton -', self.desc)
+
         print('Class -', self.accClass)
+
         print('Defense -', self.statEffect)
 
-#=============================================================================================
-#Items========================================================================================
+
+# =============================================================================================
+
+# Items========================================================================================
+
 class items(item):
     'items'
+
     def __init__(self, ID, name, weight, value, desc, effect):
         self.effect = effect
+
         super().__init__(ID, name, weight, value, desc)
+
     def itemStats(self):
         print('Name -', self.name)
+
         print('Weight -', self.weight)
+
         print('Value -', self.value)
+
         print('Descripton -', self.desc)
+
         print('Class -', self.effect)
 
-#=============================================================================================
-#actor classes================================================================================
+
+# =============================================================================================
+
+# actor classes================================================================================
+
 class actor():
     'actors'
+
     def __init__(self, name, level, essential, HP, AP):
         self.name = name
-        self.level = level      
+
+        self.level = level
+
         self.essential = essential
+
         self.HP = HP
+
         self.AP = AP
-#----------------------------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------------------------
+
 class pc(actor):
     'the player character'
+
     def __init__(self, name, subclass, level, HP, AP, Str, Dex, Int, essential, location):
         self.location = location
+
         self.subclass = subclass
+
         self.Str = Str
+
         self.Dex = Dex
+
         self.Int = Int
+
         super().__init__(name, level, essential, HP, AP)
+
     def pcStats(self):
         print('Name -', self.name)
+
         print('Class -', self.subclass)
+
         print('Level -', self.level)
+
         print('Health -', self.HP)
+
         print('Action points -', self.AP)
+
         print('Strength -', self.Str)
+
         print('Dexterity -', self.Dex)
+
         print('Intelligence -', self.Int)
+
     def encodeStats(self):
         global statsString
-        stats  = [self.name, self.subclass, self.level, self.HP, self.AP, self.Str, self.Dex, self.Int, self.essential, self.location]
+
+        stats = [self.name, self.subclass, self.level, self.HP, self.AP, self.Str, self.Dex, self.Int, self.essential,
+                 self.location]
+
         statsString = " ".join([str(i) for i in stats])
+
         saveFile = open("saveFile.txt", "w")
-        saveFile.write (statsString)
+
+        saveFile.write(statsString)
+
         saveFile.close()
-#-----------------------------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------------------------
+
 class npc(actor):
     'non-player characters'
-    def __init__(self, name, level, desc, essential, hostile, HP, AP, loot):
+
+    def __init__(self, name, level, desc, essential, hostile, HP, AP, move1, move2, move3):
         self.hostile = hostile
-        self.loot = loot
         self.desc = desc
         super().__init__(name, level, essential, HP, AP)
     def npcStats(self):
@@ -273,6 +462,19 @@ class npc(actor):
         print('hostile? -', self.hostile)
         print(self.HP)
         print(self.AP)
-        print('Drops -', self.loot)
+
+
+
+# =================================================================================================
 #=================================================================================================
+#Moves classes====================================================================================
+class move():
+    """moves"""
+    def __init__(self, name, OpponentHPDamage, OpponentAPDamage, PlayerHPDamage, PlayerAPDamage):
+        self.name = name
+        self.OpponentHPDamage = OpponentHPDamage
+        self.OpponentAPDamage = OpponentAPDamage
+        self.PlayerHPDamage = PlayerHPDamage
+        self.PlayerAPDamage = PlayerAPDamage
+
 
--- items.py
+++ items.py
@@ -1,5 +1,45 @@
 import classes
+#Moves======================================================================
+mv0001 = classes.move(name="slash",
+                      OpponentHPDamage=-10,
+                      OpponentAPDamage=0,
+                      PlayerHPDamage=0,
+                      PlayerAPDamage=-5)
+mv0002 = classes.move(name="stab",
+                      OpponentHPDamage=-20,
+                      OpponentAPDamage=0,
+                      PlayerHPDamage=0,
+                      PlayerAPDamage=-25)
+mv0003 = classes.move(name="bash",
+                      OpponentHPDamage=-5,
+                      OpponentAPDamage=-20,
+                      PlayerHPDamage=0,
+                      PlayerAPDamage=-10)
+mv0004 = classes.move(name="evade",
+                      OpponentHPDamage=0,
+                      OpponentAPDamage=0,
+                      PlayerHPDamage=0,
+                      PlayerAPDamage=-18)
+mv0005 = classes.move(name="fireball",
+                      OpponentHPDamage=-20,
+                      OpponentAPDamage=0,
+                      PlayerHPDamage=0,
+                      PlayerAPDamage=-20)
+mv0006 = classes.move(name="frostfall",
+                      OpponentHPDamage=-5,
+                      OpponentAPDamage=-20,
+                      PlayerHPDamage=0,
+                      PlayerAPDamage=-15)
+mv0007 = classes.move(name="heal",
+                      OpponentHPDamage=0,
+                      OpponentAPDamage=0,
+                      PlayerHPDamage=+30,
+                      PlayerAPDamage=-20)
+
+import classes
+
 #Armour=======================================================================
+
 ha0001 = classes.heavyArmour(ID = 'ha0001',
                              name = 'Iron Armor',
                              weight = 10,
@@ -16,118 +56,224 @@ la0001 = classes.lightArmour(ID = 'la0001',
                              apparelClass = 'light',
                              defense = 5,
                              effects = 'none')
+
 cl0001 = classes.clothing(ID = 'cl0001',
                           name = 'Apprentice Robes',
-                             weight = 1,
-                             value = 100,
-                             desc = 'A simple set of robes',
-                             apparelClass = 'clothing',
-                             defense = 0,
-                             effects = 'none')
+                          weight = 1,
+                          value = 100,
+                          desc = 'A simple set of robes',
+                          apparelClass = 'clothing',
+                          defense = 0,
+                          effects = 'none')
+
 #Weapons======================================================================
+
 hw0001 = classes.heavyWeapon(ID = 'hw0001',
+
                              name = 'Iron Sword',
+
                              weight = 10,
+
                              value = 100,
+
                              desc = 'A simple iron sword',
+
                              weaponClass = 'heavy',
+
                              effects = 'none',
+
                              move1 = 'slash',
+
                              move2 = 'stab',
+
                              move3 = 'bash',
+
                              StrReq = 10,
+
                              DexReq = 4,
+
                              IntReq = 0)
+
 lw0001 = classes.lightWeapon(ID = 'lw0001',
+
                              name = 'Iron Dagger',
+
                              weight = 2,
+
                              value = 100,
+
                              desc = 'A simple iron dagger',
+
                              weaponClass = 'light',
+
                              effects = 'none',
+
                              move1 = 'slash',
+
                              move2 = 'stab',
+
                              move3 = 'evade',
+
                              StrReq = 4,
+
                              DexReq = 10,
+
                              IntReq = 0)
+
 mw0001 = classes.magicWeapon(ID = 'mw0001',
+
                              name = 'wood staff',
+
                              weight = 7,
+
                              value = 100,
+
                              desc = 'A simple sorcerer staff',
+
                              weaponClass = 'magic',
+
                              effects = 'none',
+
                              move1 = 'fireball',
+
                              move2 = 'frostfall',
+
                              move3 = 'magicshield',
+
                              StrReq = 10,
+
                              DexReq = 4,
+
                              IntReq = 0)
+
 #Accessories==================================================================
+
 ac0001 = classes.accessories(ID = 'ac0001',
+
                              name = 'ruby ring',
+
                              weight = 1,
+
                              value = 500,
+
                              desc = 'a silver ring with a ruby inside',
+
                              accClass = 'ring',
+
                              statEffect = '+10 max hp')
+
 ac0002 = classes.accessories(ID = 'ac0002',
+
                              name = 'travellers cloak',
+
                              weight = 5,
+
                              value = 50,
+
                              desc = 'a brown, dusty cloak',
+
                              accClass = 'cloak',
+
                              statEffect = '+2 dexterity')
+
 ac0003 = classes.accessories(ID = 'ac0003',
+
                              name = 'mystic pendant',
+
                              weight = 3,
+
                              value = 500,
+
                              desc = 'a mystic pendant with ancient words inscribed on it',
+
                              accClass = 'pendant',
+
                              statEffect = '+30 max ap')
+
 #Items========================================================================
+
 it0001 = classes.items(ID = 'it0001',
+
                              name = 'health potion',
+
                              weight = 2,
+
                              value = 250,
+
                              desc = 'a weak health potion',
+
                              effect = '+30 hp')
+
 it0002 = classes.items(ID = 'it0001',
+
                              name = 'mana potion',
+
                              weight = 2,
+
                              value = 300,
+
                              desc = 'a weak mana potion',
+
                              effect = '+40 ap')
+
 #Currency======================================================================
+
 cu0001 = classes.currency(ID = 'cu0001',
+
                           name ='gold',
+
                           weight = 0,
+
                           value = 1,
+
                           desc = 'The common currency in the world of Thule.')
+
 cu0002 = classes.currency(ID = 'cu0002',
+
                           name='Diamond',
+
                           weight=0.5,
+
                           value=600,
+
                           desc='The clear reflections in the diamond make it the ultimate jewel for the wealthy.')
+
 cu0003 = classes.currency(ID = 'cu0003',
+
                           name='Ruby',
+
                           weight=0.2,
+
                           value=350,
+
                           desc='The shining allure of the Ruby makes it popular with all wealth classes, and into all jewelers shops.')
+
 cu0004 = classes.currency(ID = 'cu0004',
+
                           name='Sapphire',
+
                           weight=0.1,
+
                           value=250,
+
                           desc='The blue of the Sapphire reflects the allure of the sky, making it popular in use for jewelry.')
 
+
+#Npc's=======================================================================
 npc0001 = classes.npc(name = '',
+
                       level = ,
+
                       desc = ,
+
                       essential = '',
+
                       hostile = '',
+
                       HP = ,
+
                       AP = ,)
+
 #NOTE: if you wanna add quest items and such they'll go here
 
 
